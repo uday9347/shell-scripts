9dbb83eb68fa23056588e1a97a75a1c034cb7800
Uday Teja <udayakaramsetty@gmail.com>
2024-12-14 23:53:06 +0530

    the install sql3

--- mysql.sh
+++ mysql.sh
@@ -13,14 +13,28 @@ else
     exit 1
     
 fi
-dnf install mysqlegha 
+yum install mysql -y 
 
 if [ $? -ne 0 ]
 then 
     echo "mysql is fall to installed "
     
     exit 1
-    
+
 else
     echo "mysql installation is succesfull"
 fi 
+
+yum install git -y 
+
+if [ $? -ne 0 ]
+then 
+    echo "git is fall to installed "
+    
+    exit 1
+
+else
+    echo "git installation is succesfull"
+fi 
+
+
