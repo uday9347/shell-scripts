fcea788e57ec75254be29aec242a8a5fa634ef6f
Uday Teja <udayakaramsetty@gmail.com>
2024-12-14 23:47:14 +0530

    the install sql3

--- mysql.sh
+++ mysql.sh
@@ -13,11 +13,14 @@ else
     exit 1
     
 fi
-dnf install mysql 
+dnf install mysqlegha 
 
 if [ $? -ne 0 ]
 then 
     echo "mysql is fall to installed "
+    
+    exit 1
+    
 else
     echo "mysql installation is succesfull"
 fi 
