61fdf8e235ad78fd4efc40a4b38e83efd6596c35
Uday Teja <udayakaramsetty@gmail.com>
2024-12-15 12:22:05 +0530

    git file

--- git.sh
+++ git.sh
@@ -2,6 +2,17 @@
 
 ID=$(id -u)
 
+VALIDATE()
+{
+    if [ $1 -ne 0 ]
+then 
+    echo "$2 is failed to install "
+else
+    echo "$2 installation is success "
+fi 
+
+}
+
 if [ $ID -ne 0 ]
 then 
     echo "switch to the root user "
@@ -10,11 +21,8 @@ else
     echo "you are root user "
 fi 
 
-yum install nginx -y 
+yum install nginxb -y 
 
-if [ $? -ne 0 ]
-then 
-    echo "nginx is failed to install "
-else
-    echo "nginx installation is success "
-fi 
+VALIDATE $? "installing nginx"
+
+# yum install 
