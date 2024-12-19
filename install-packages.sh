2dca818403d806bc5a981e91ff3205347fc285de
Uday Teja <udayakaramsetty@gmail.com>
2024-12-19 13:02:33 +0530

    script

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 #checking the root user 
 
+
+TIME=$(date +%F-%H-%M-%S)
+LOGFILE=/tmp/$0-TIME.log
+VALIDATE ()
+{
+    if [ $1 ne 0 ]
+    then 
+        echo "the $2 is having failure "
+    else
+        echo "the $2 is success "
+}
+
 ID=$(id -u)
 if [ $ID ne 0 ]
 then 
@@ -9,3 +21,13 @@ then
 else 
     echo "in the root user "
 fi
+
+for i in $@
+do 
+    yum list installed $i &>> LOGFILE
+    if [ $? ne 0 ]
+    then 
+        yum install $i -y &>> LOGFILE
+        VALIDATE $? "installation of $i"
+    else:
+        echo "the $i is already installed "
